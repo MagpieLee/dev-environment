558ef569ba96c61cbf9a49726803f989ed7d2130
MagpieLee <donghwi.lee@magpiesys.com>
2023-02-04 21:43:04 +0000

    Add vscode extensions script.

---
+++ leedh/install-vscode-extensions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+code-server --install-extension dbaeumer.vscode-eslint
+code-server --install-extension esbenp.prettier-vscode
+code-server --install-extension ms-python.isort
+code-server --install-extension ms-python.python
+code-server --install-extension svelte.svelte-vscode
+
